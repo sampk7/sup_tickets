3e9bdaa3bca00aa8cfb1079c53341b0d1c1547f4
sampk7 <samuelpkelbiso@gmail.com>
2026-04-16 09:25:59 +0300

    removed debug banner

--- lib/app.dart
+++ lib/app.dart
@@ -35,6 +35,7 @@ class SupportTicketApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       theme: appTheme,
       routerConfig: router,
     );
